bc537d6485491aaf1f215f8f5d58729cdc016adb
bnoden <brandroid369@gmail.com>
2018-01-11 18:36:22 -0600

    refactor

--- OffAndUp.js
+++ OffAndUp.js
@@ -1,7 +1,26 @@
+let stateCheck = setInterval(() => {
+  if (document.readyState === 'complete') {
+    setItemPage();
+    identifySoldItems();
+    clearInterval(stateCheck);
+  }
+}, 100);
+
+const items = () => document.querySelectorAll('.vertical-middle');
+const prices = () => document.querySelectorAll('.item-info-price');
+let itemsLength = items().length;
+let pricesLength = prices().length;
+
 setInterval(() => {
-  setItemPage();
-  identifySoldItems();
-}, 200);
+  if (itemsLength !== items().length) {
+    itemsLength = items().length;
+    setItemPage();
+  }
+  if (pricesLength !== prices().length) {
+    pricesLength = items().length;
+    identifySoldItems();
+  }
+}, 100);
 
 const itemContainer = document.createElement('div');
 const bufferLayer = document.createElement('div');
